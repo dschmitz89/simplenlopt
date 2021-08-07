b5c820cc7061c98317a9d2a7980bb09743e654b2
dschmitz89 <danielschmitzsiegen@gmail.com>
2021-08-07 11:57:19 +0200

    Add description in setup file for PyPI

--- setup.py
+++ setup.py
@@ -1,15 +1,22 @@
 from setuptools import setup
+from os import path
+
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
 
 if __name__ == '__main__':
     
     setup(
         name='simplenlopt',   
-        version='1.0', 
+        version='1.0.1', 
         description='A scipy.optimize like API for nlopt',
         url='https://github.com/dschmitz89/simplenlopt/',
         download_url='https://github.com/dschmitz89/simplenlopt/archive/refs/tags/1.0.tar.gz',
         author='Daniel Schmitz',
-        license='MIT',
+        license='Apache 2',
+        long_description=long_description,
+        long_description_content_type='text/markdown',
         packages=['simplenlopt'],
         install_requires=[
             'numpy',
